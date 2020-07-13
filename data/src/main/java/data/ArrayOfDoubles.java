a54ca046a5b4677798c3fa93b75c4182a80f6d90
Alina <alinablago19@gmail.com>
2020-07-13 21:52:08 +0300

    New commit

--- data/src/main/java/data/ArrayOfDoubles.java
+++ data/src/main/java/data/ArrayOfDoubles.java
@@ -5,13 +5,17 @@ import impl.TaskServiceImpl;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
 public class ArrayOfDoubles extends TaskServiceImpl {
     static void arrayOfDoubles (double[] array) {
 
         Stream<Double> stream1 = (Stream<Double>) Arrays.stream(array);
-        stream1.mapToDouble(Double::doubleValue).max().getAsDouble();
-        stream1.mapToDouble(Double::doubleValue).min().getAsDouble();
+        double max = stream1.mapToDouble(Double::doubleValue).max().getAsDouble();
+        double min = stream1.mapToDouble(Double::doubleValue).min().getAsDouble();
+        double sum = stream1.mapToDouble(Double::doubleValue).sum();
+        OptionalDouble average = stream1.mapToDouble(Double::doubleValue).average();
+        System.out.println(max + " " + min + " " + sum + " " + average);
     }
 }
