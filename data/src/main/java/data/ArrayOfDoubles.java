bae2ff410f4c358d435cf230709194095497b792
Alina <alinablago19@gmail.com>
2020-07-13 21:47:08 +0300

    First commit

--- data/src/main/java/data/ArrayOfDoubles.java
+++ data/src/main/java/data/ArrayOfDoubles.java
@@ -9,18 +9,9 @@ import java.util.stream.Stream;
 
 public class ArrayOfDoubles extends TaskServiceImpl {
     static void arrayOfDoubles (double[] array) {
-        //double[] array = {"Java", "Ruuuuussshhh"};
-        //Stream<Double> streamOfArray = Arrays.stream(array);
-        //return Arrays.stream(array)
 
-        List<String> list = Arrays.stream(array).min(Double::compare);
-
-        //Optional<Double> min =  Arrays.stream(array).min(Double::compare);
-       // Optional<Double> max =  Arrays.stream(array).max(Double::compare);
-        System.out.println(min.get());
-        System.out.println(max.get());
-
-        double max = Arrays.stream(array).mapToDouble(Double::doubleValue).max().getAsDouble();//16.0
-        double min = Arrays.stream(array).mapToDouble(Double::doubleValue).min().getAsDouble();//-6.0
+        Stream<Double> stream1 = (Stream<Double>) Arrays.stream(array);
+        stream1.mapToDouble(Double::doubleValue).max().getAsDouble();
+        stream1.mapToDouble(Double::doubleValue).min().getAsDouble();
     }
 }
